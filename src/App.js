f83771b811ea00cc4bae09d84ec05b4e46dc1eb4
Anthony Coman <acoman@yahoo.com>
2021-12-17 13:22:05 -0600

    added decimal functionality

--- src/App.js
+++ src/App.js
@@ -8,33 +8,33 @@ function App() {
   const [operation, setOperation] = useState('');
 
   const handleClick=(e)=>{
-    setCurrentValue(parseInt(currentValue+e.target.textContent));
+    setCurrentValue(parseFloat(currentValue+e.target.textContent));
   }
   const handlePlus=(e)=>{
-    setPreviousValue(currentValue);
+    setPreviousValue(parseFloat(currentValue));
     setCurrentValue(0);
     setOperation('+');
   }
   const handleMinus=(e)=>{
-    setPreviousValue(currentValue);
+    setPreviousValue(parseFloat(currentValue));
     setCurrentValue(0);
     setOperation('-');
   }
   const handleMultiplication=(e)=>{
-    setPreviousValue(currentValue);
+    setPreviousValue(parseFloat(currentValue));
     setCurrentValue(0);
     setOperation('*');
+    console.log(previousValue);
   }
   const handleDivision=(e)=>{
-    setPreviousValue(currentValue);
+    setPreviousValue(parseFloat(currentValue));
     setCurrentValue(0);
     setOperation('/');
   }
   const handleClear=(e)=>{
     setCurrentValue(0);
-    setPreviousValue('')
+    setPreviousValue([])
     setOperation('')
-    console.log('clear!');
   }
   const handleOutput=(e)=>{
     if(operation==='+'){
@@ -46,11 +46,14 @@ function App() {
     } else if(operation==='/'){
       setCurrentValue(previousValue/currentValue);
     }
-    console.log('Computate!');
+  }
+  const handlePeriod=(e)=>{
+    setCurrentValue(currentValue+'.');
   }
   return (
     <main>
       <h1>Calculator</h1>
+      {/* {operation && <h3>{previousValue[previousValue.length-2]} {operation} {previousValue[previousValue.length-1]} =</h3>} */}
       <div className='serial'>
         <input className='box' value={currentValue} type='text' />
       </div>
@@ -60,7 +63,10 @@ function App() {
           <h2 className='operation' onClick={handleMultiplication}>*</h2>
           <h2 className='operation' onClick={handleDivision}>/</h2>
         </div>
-        <div className='numbers'>
+        <div className='numbers'>          
+          <h2 className='number' onClick={handleClear}>C</h2>
+          <h2 className='number' onClick={handlePeriod}>.</h2>
+          <h2 className='number' onClick={handleOutput}>=</h2>
           <h2 className='number' onClick={handleClick}>1</h2>
           <h2 className='number' onClick={handleClick}>2</h2>
           <h2 className='number' onClick={handleClick}>3</h2>
@@ -69,10 +75,9 @@ function App() {
           <h2 className='number' onClick={handleClick}>6</h2>
           <h2 className='number' onClick={handleClick}>7</h2>
           <h2 className='number' onClick={handleClick}>8</h2>
-          <h2 className='number' onClick={handleClick}>9</h2>
-          <h2 className='number' onClick={handleClear}>C</h2>
+          <h2 className='number' onClick={handleClick}>9</h2>          
           <h2 className='number' onClick={handleClick}>0</h2>
-          <h2 className='number' onClick={handleOutput}>=</h2>
+          
         </div>
     </main>
   );
